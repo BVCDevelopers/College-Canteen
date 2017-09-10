705a6ccd3ff7bd65d69bc01b6db816609c820fb4
Suzal3579 <pe.messh@gmail.com>
2017-09-10 10:27:37 +0530

    Setting up the project ... Partials and layouts ...

--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,6 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const data = {
-        shopName: req.body.shopName,
-        item: {
-            itemName: req.body.itemName,
-            price: req.body.price,
-            category: req.body.category
-        }
-    };
-    canteenModel.create(data)
-        .then((result) => {
-            res.send({ result });
-        });
 });
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -4,11 +4,6 @@ const router = express.Router();
 const canteenModel = require('../models/canteenModel');
 
 router.get('/:shop', (req, res, next) => {
-    const shop = req.params.shop;
-    canteenModel.find({ shopName: shop })
-        .then((results) => {
-            res.render('shop', { results: results });
-        });
 });
 
 module.exports = router;
