fcb3e03c8ec1beecd8c4575ed64885c02f0cab1a
Suzal3579 <pe.messh@gmail.com>
2017-09-12 22:41:58 +0530

    Routes protection done ... shop login signup sign all working fine for now ...

--- routes/shop.js
+++ routes/shop.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
+// yo method ko bichha ko le route lai protect mattra garrxa ... 
+// Method ko reference yeta xa .. ie do not execute here ...
+router.get('/shopMenu', isLoggedIn, (req, res, next) => {
+    res.render("shop/shopMenu");
+});
+
 router.post('/signup', passport.authenticate('local.signup', {
     successRedirect: "/shop/shopMenu",
     failureRedirect: "/shopSignUp",
@@ -14,8 +20,18 @@ router.post('/signin', passport.authenticate('local.signin', {
     failureFlash: true
 }));
 
-router.get('/shopMenu', (req, res, next) => {
-    res.render("shop/shopMenu");
+router.get('/logout', (req, res, next) => {
+    // passport provides logout function ...
+    req.logOut();
+    res.redirect('/');
 });
 
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+    // isAuthenticated() is provided in passport using sessions ...
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
